8dbb06cb4b3c53f699e33a3572bfe5584213fd68
Luka <lukamasulovic2@gmail.com>
2025-02-18 01:24:59 +0100

    h

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'password',
         'address',
         'phone_number',
         'epcg_naplatni_broj',
